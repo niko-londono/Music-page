51d05c0731d6d5add17e19012c9a9a4549eac1b0
Nicolas Londoño Hernandez <92286646+niko-londono@users.noreply.github.com>
2023-02-12 17:04:44 -0400

    Delete Tarea 3 directory

--- Tarea 3/js/java.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
-    loader.addEventListener("animationend", () => {
-      document.body.removeChild(loader);
-    });
-  });
